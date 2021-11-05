0cd356afbbb146e5ff60f60aa3f548b50aeb12ba
AnantDuhan <duhananant@gmail.com>
2021-11-05 22:39:03 +0530

    Connected to MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,18 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('realkart-db', 'root', 'AnantDuhan@0911##', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://anantduhan:Sapna-0911@realkart.ciloc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected to MongoDB");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+ 
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
