733f6e6aef7455ff93bbfa3d6e50a72c6cf862cc
AnantDuhan <duhananant@gmail.com>
2021-11-08 20:55:54 +0530

    Fetching All Products

--- util/database.js
+++ util/database.js
@@ -1,18 +1,30 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
     "mongodb+srv://anantduhan:Sapna-0911@realkart.ciloc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   )
     .then((client) => {
-      console.log("Connected to MongoDB");
-      callback(client);
+      console.log("Connected to MongoDB!!!");
+      _db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
- 
+};
+
+const getDb = () => {
+  if(_db) {
+    return _db;
+  }
+  throw 'No Database Found!!';
+
 }
 
-module.exports = mongoConnect;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
