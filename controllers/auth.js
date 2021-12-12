50fa6f3fcefe1ab099ba67e986df998ebf4e9971
AnantDuhan <duhananant@gmail.com>
2021-12-12 13:17:47 +0530

    Sending Emails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "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",
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -68,7 +79,10 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
-        req.flash("error", "E-mail exists already, please pick a different one.");
+        req.flash(
+          "error",
+          "E-Mail exists already, please pick a different one."
+        );
         return res.redirect("/signup");
       }
       return bcrypt
@@ -83,6 +97,16 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect("/login");
+          return transporter.sendMail({
+            to: email,
+            from: "duhananant@gmail.com",
+            subject: "Signup succeeded!",
+            html: "<h1>Thank you for choosing RealKart</h1>",
+            text: "Successfully Signed up",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
     })
     .catch((err) => {
