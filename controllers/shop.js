98465197c04aebb5fcd7a657e9875ad55f5eb3ae
AnantDuhan <duhananant@gmail.com>
2021-12-20 19:53:42 +0530

    Generating Invoice.pdf

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const pdfDocument = require('pdfkit');
+
 const Product = require("../models/product");
 const Order = require("../models/order");
 
@@ -131,6 +133,37 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = "invoice-" + orderId + ".pdf";
       const invoicePath = path.join("data", "invoices", invoiceName);
+
+      const pdfDoc = new PdfDocument();
+      res.setHeader(
+        "Content-Disposition",
+        'inline; filename="' + invoiceName + '"'
+      );
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.fontSize(26).text('Invoice', {
+        underline: true
+      });
+      pdfDoc.text('---------------------------');
+      let totalprice = 0;
+      order.products.forEach(prod => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)  
+          .text(
+            prod.product.title + 
+            ' - ' + 
+            prod.quantity + 
+            ' x ' + 
+            '$' + 
+            prod.product.price
+          );
+      });
+      pdfDoc.text('-----');
+      pdfDoc.fontSize(20).text('Total Price: $' + totalprice);
+
+      pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
       //     return next(err);
@@ -139,10 +172,7 @@ exports.getInvoice = (req, res, next) => {
       //   res.send(data);
       // });
       const file = fs.createReadStream(invoicePath);
-      res.setHeader(
-        "Content-Disposition",
-        'inline; filename="' + invoiceName + '"'
-      );
+
       file.pipe(res);
     })
     .catch(err => next(err));
