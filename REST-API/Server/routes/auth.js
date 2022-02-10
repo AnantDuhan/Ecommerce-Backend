bfe911cc438b70f06dbcb5b48d8505ba01f842d9
AnantDuhan <duhananant@gmail.com>
2022-02-10 15:57:59 +0530

    Added SignUp Validation

--- REST-API/Server/routes/auth.js
+++ REST-API/Server/routes/auth.js
@@ -1,6 +1,31 @@
 const express = require("express");
-const router = require("./feed");
+const { body } = require("express-validator");
 
-router.put('/signup');
+const User = require("../models/user");
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+router.put('/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email')
+        .custom((value, { req }) => {
+            return User
+                .findOne({ email: value })
+                .then(userDoc => {
+                    if(userDoc) {
+                        return Promise.reject('E-mail address already exists!');
+                    }
+                })
+        })
+        .normalizeEmail(),
+        body('password')
+            .trim()
+            .isLength({ min: 8 }),
+        body('name').trim().not().isEmpty()
+    ]
+    , authController.signup
+);
 
 module.exports = router;
